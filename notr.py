e1b465cd493456e3ee180e8b82399a89e22eade6
cepthomas <cepthomas@gmail.com>
2026-07-25 14:04:31 -0500

    WIP.

--- notr.py
+++ notr.py
@@ -12,8 +12,6 @@ import sublime
 import sublime_plugin
 from . import sbot_common as sc
 
-# TODO Need a help which opens the example file(s).
-
 # Known file types.
 IMAGE_TYPES = ['.jpg', '.jpeg', '.png', '.bmp', '.gif']
 
